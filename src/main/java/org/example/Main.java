91c963b3b4cab5abb3ddc8b09cf448274cbd5607
Aman Sharma <mannu.poski10@gmail.com>
2024-08-16 19:07:48 +0200

    Inital commit for croaker

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,105 @@
+package org.example;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.JarFile;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.zip.ZipEntry;
+
+public class Main {
+    public static final Path ROOT = Paths.get(".").toAbsolutePath().normalize().getParent();
+    private static final Path OUTPUT;
+    private static final Logger logger;
+    static {
+        // create folder for all the classfiles
+        OUTPUT = ROOT.resolve("EQ");
+        if (!OUTPUT.toFile().exists()) {
+            OUTPUT.toFile().mkdir();
+        }
+
+        logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+
+        try {
+
+            // This block configure the logger with handler and formatter
+            fh = new FileHandler(ROOT.resolve("dataset.log").toString());
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static final Path DATASET = ROOT.resolve("dataset");
+    public static void main(String[] args) throws IOException {
+        Path datasetEQ = DATASET.resolve("EQ.tsv");
+        Reader reader = new FileReader(datasetEQ.toFile());
+
+        FileWriter f0 = new FileWriter("paths.csv");
+
+        String newLine = System.getProperty("line.separator");
+
+        Iterable<CSVRecord> records = CSVFormat.MONGODB_TSV.parse(reader);
+        for (CSVRecord record : records) {
+            long recordNumber = record.getRecordNumber();
+            if (recordNumber == 1) {
+                // title row
+                continue;
+            }
+            Path pathToJar1 = DATASET.resolve(Paths.get(record.get(0)));
+            Path pathToJar2 = DATASET.resolve(Paths.get(record.get(1)));
+
+            String classfileInsideJar1 = record.get(2);
+            String classfileInsideJar2 = record.get(3);
+
+            assert classfileInsideJar1 != null && classfileInsideJar1.equals(classfileInsideJar2);
+
+            JarFile jar1 = new JarFile(pathToJar1.toFile());
+            JarFile jar2 = new JarFile(pathToJar2.toFile());
+
+            byte[] classfileBytes1 = getClassFileBytes(jar1, classfileInsideJar1);
+            byte[] classfileBytes2 = getClassFileBytes(jar2, classfileInsideJar2);
+            String classfileName1 = getClassNameFromFullyQualifiedClassName(classfileInsideJar1);
+            String classfileName2 = getClassNameFromFullyQualifiedClassName(classfileInsideJar2);
+            Path outputClassfile1 = OUTPUT.resolve(String.valueOf(recordNumber)).resolve("first").resolve(classfileName1);
+            Path outputClassfile2 = OUTPUT.resolve(String.valueOf(recordNumber)).resolve("second").resolve(classfileName2);
+
+            Files.createDirectories(outputClassfile1.getParent());
+            Files.write(outputClassfile1, classfileBytes1);
+
+            Files.createDirectories(outputClassfile2.getParent());
+            Files.write(outputClassfile2, classfileBytes2);
+
+            f0.write(outputClassfile1.toString() + "," + outputClassfile2.toString() + newLine);
+
+
+            logger.info("Record Number: " + recordNumber + " " + outputClassfile1 + " " + outputClassfile2);
+        }
+        f0.close();
+    }
+
+    private static byte[] getClassFileBytes(JarFile jar, String classfile) throws IOException {
+        String sanitizedClassfile = classfile.substring(1);
+        ZipEntry entry = jar.getEntry(sanitizedClassfile);
+        return jar.getInputStream(entry).readAllBytes();
+    }
+
+    private static String getClassNameFromFullyQualifiedClassName(String fullyQualifiedClassName) {
+        return fullyQualifiedClassName.substring(fullyQualifiedClassName.lastIndexOf('/') + 1);
+    }
+}
