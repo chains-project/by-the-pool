d33fd90efa56a920c810bc4a60bf109ace9c820e
Aman Sharma <mannu.poski10@gmail.com>
2024-08-20 11:41:12 +0200

    perf: output all files at the end of execution

--- src/main/java/org/example/Diffoscope.java
+++ src/main/java/org/example/Diffoscope.java
@@ -1,35 +1,44 @@
 package org.example;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.io.FileReader;
-import java.lang.InterruptedException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
 
 import static org.example.Constants.ROOT;
 
 public class Diffoscope {
 
+    public static final Map<String, Map.Entry<String, String>> diffoscopeMap = new ConcurrentHashMap<>();
+
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        Path paths = ROOT.resolve("paths.csv");
+        Path paths = ROOT.resolve("try.csv");
         Reader reader = new FileReader(paths.toFile());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(reader);
 
 
         int workers = Runtime.getRuntime().availableProcessors();
-        ExecutorService pool= Executors.newFixedThreadPool(workers);
+        ExecutorService pool = Executors.newFixedThreadPool(workers);
 
         for (CSVRecord record : records) {
             pool.submit(new DiffoscopeDiff(record));
         }
         pool.shutdown();
         pool.awaitTermination(1, TimeUnit.HOURS);
+
+        for (Map.Entry<String, Map.Entry<String, String>> entry : diffoscopeMap.entrySet()) {
+            Files.writeString(ROOT.resolve("output").resolve(entry.getKey()).resolve("diffoscope.diff"), entry.getValue().getValue());
+            Files.writeString(ROOT.resolve("output").resolve(entry.getKey()).resolve(".exitcode"), entry.getValue().getKey());
+        }
     }
 }
--- src/main/java/org/example/DiffoscopeDiff.java
+++ src/main/java/org/example/DiffoscopeDiff.java
@@ -2,33 +2,44 @@ package org.example;
 
 import org.apache.commons.csv.CSVRecord;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Logger;
+import java.util.Map;
+
+import static org.example.Diffoscope.diffoscopeMap;
 
 public class DiffoscopeDiff implements Runnable {
     private static final Path DIFFOSCOPE = Constants.DIFFOSCOPE_EXECUTABLE;
 
-    private static final Logger logger = Constants.getLogger(DiffoscopeDiff.class, "diffoscope.log");
 
     private final CSVRecord record;
+
     public DiffoscopeDiff(CSVRecord record) {
-        this.record=record;
+        this.record = record;
 
     }
+
     public void run() {
         Path class1 = Paths.get(record.get(0));
         Path class2 = Paths.get(record.get(1));
 
-        ProcessBuilder pb = new ProcessBuilder(DIFFOSCOPE.toString(), "--text", "diffoscope.diff", class1.toString(), class2.toString());
+        ProcessBuilder pb = new ProcessBuilder(DIFFOSCOPE.toString(), class1.toString(), class2.toString());
         pb.directory(class1.getParent().getParent().toFile());
+
 
         String recordNumber = class1.getParent().getParent().getFileName().toString();
         try {
             Process p = pb.start();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             int exitCode = p.waitFor();
-            logger.info("Record: " + recordNumber + " Exit Code: " + exitCode);
+            StringBuilder sb = new StringBuilder();
+            for (String line; (line = br.readLine()) != null; ) {
+                sb.append(line).append("\n");
+            }
+            diffoscopeMap.put(recordNumber, Map.entry(String.valueOf(exitCode), sb.toString()));
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
