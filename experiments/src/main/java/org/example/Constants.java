4d6109f624f0f65bfae7fd42d60ba2911c989a0c
Aman Sharma <mannu.poski10@gmail.com>
2024-09-03 20:50:11 +0200

    chore(experiments): optimize logs

--- experiments/src/main/java/org/example/Constants.java
+++ experiments/src/main/java/org/example/Constants.java
@@ -14,6 +14,8 @@ public class Constants {
     public static final Properties PROPERTIES;
 
     static {
+        System.setProperty(
+                "java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
         PROPERTIES = new Properties();
         try {
             // load a properties file from class path, inside static method
